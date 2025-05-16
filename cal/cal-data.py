730056e6f548812b5038c468a2fdc7716a4db8f3
tiagof2424 <fortinitiago098@gmail.com>
2025-05-16 20:58:04 +0000

    files cal

---
+++ cal/cal-data.py
@@ -0,0 +1,6 @@
+num1 = 1
+num2 = 3
+op = "+"
+result = eval(f"{num1}{op}{num2}")
+
+print(result)
---
+++ cal/cal-input.py
@@ -0,0 +1,6 @@
+# VARIABLES
+num1 = "n1"=input
+num2 = "n2"=input
+op = "+"=input
+eval(f"{n1){op}{na}")
+# resultado en pantalla
