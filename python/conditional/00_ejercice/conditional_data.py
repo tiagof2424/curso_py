7065a5036ed7c75835c92b0d31342786cc66511c
tiagof2424 <fortinitiago098@gmail.com>
2025-08-28 21:25:14 +0000

    conditional ejercice

--- python/conditional/00_ejercice/conditional_data.py
+++ python/conditional/00_ejercice/conditional_data.py
@@ -1,3 +1,8 @@
 # tres chicos quieren comprar una consola, pero hay un problema, los precios no estan alado de los estantes, ellos quieren comprar la consola mas cara.
 # tenemos que solicitar el dinero que tiene cada uno, si hay dos consolas con el mismo precio debemos mostrar las dos consolas y mostrar el vuelto por si les queda
 trolintrolin_cash = 100.000
+
+console = {
+    "ps8": 200.000,
+    "": 
+}
